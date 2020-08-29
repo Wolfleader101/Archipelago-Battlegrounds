d68eeafc639b489ef77761ca47141ca61f212db7
Wolfleader <james.morgan@iinet.net.au>
2020-08-29 20:00:29 +0800

    interesting

--- Assets/Scripts/BaseEntity.cs
+++ Assets/Scripts/BaseEntity.cs
@@ -8,9 +8,10 @@ public class BaseEntity : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
-
+    
     public BaseWeapon weapon;
     public Transform enemy;
+    
     public int targetDistance = 10;
     public int outOfRange = 20;
     public float speed = 10;
@@ -28,14 +29,28 @@ public class BaseEntity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
         float dist = Vector2.Distance(transform.position, enemy.position);
         if (dist <= targetDistance && dist <= outOfRange)
         {
             transform.up = enemy.position - transform.position;
             weapon.Shoot();
 
-            //Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-            //rb.AddForce(enemy.position * speed);
+            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+
+            if (currentHealth > 50)
+            {
+                rb.AddForce(enemy.position * speed);
+            }
+            else
+            {
+                rb.AddForce(-enemy.position * speed);
+            }
+            
         }
     }
 
